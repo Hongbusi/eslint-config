a5fb53d22de0912d5304f50e82d45d70620431a3
Hongbusi <q951686662@qq.com>
2022-03-09 18:03:49 +0800

    feat: update basic

--- packages/eslint-config-basic/index.js
+++ packages/eslint-config-basic/index.js
@@ -6,99 +6,7 @@ module.exports = {
   },
   extends: [
     'standard',
-    'plugin:import/recommended',
-    'plugin:eslint-comments/recommended',
-    'plugin:jsonc/recommended-with-jsonc',
-    'plugin:yml/standard'
-  ],
-  plugins: [
-    'html',
-    'unicorn'
-  ],
-  settings: {
-    'import/resolver': {
-      node: { extensions: ['.js', '.mjs', '.ts', '.d.ts'] }
-    }
-  },
-  overrides: [
-    {
-      files: ['*.json', '*.json5'],
-      parser: 'jsonc-eslint-parser',
-      rules: {
-        'quotes': ['error', 'double'],
-        'quote-props': ['error', 'always'],
-        'comma-dangle': ['error', 'never']
-      }
-    },
-    {
-      files: ['*.yaml', '*.yml'],
-      parser: 'yaml-eslint-parser'
-    },
-    {
-      files: ['package.json'],
-      parser: 'jsonc-eslint-parser',
-      rules: {
-        'jsonc/sort-keys': [
-          'error',
-          {
-            pathPattern: '^$',
-            order: [
-              'name',
-              'version',
-              'description',
-              'keywords',
-              'license',
-              'repository',
-              'funding',
-              'author',
-              'type',
-              'files',
-              'exports',
-              'main',
-              'module',
-              'unpkg',
-              'bin',
-              'scripts',
-              'husky',
-              'lint-staged',
-              'peerDependencies',
-              'peerDependenciesMeta',
-              'dependencies',
-              'devDependencies',
-              'eslintConfig'
-            ]
-          },
-          {
-            pathPattern: '^(?:dev|peer|optional|bundled)?[Dd]ependencies$',
-            order: { type: 'asc' }
-          }
-        ]
-      }
-    },
-    {
-      files: ['*.d.ts'],
-      rules: {
-        'import/no-duplicates': 'off'
-      }
-    },
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/no-var-requires': 'off'
-      }
-    },
-    {
-      files: ['scripts/**/*.*'],
-      rules: {
-        'no-console': 'off'
-      }
-    },
-    {
-      files: ['*.test.ts', '*.test.js', '*.spec.ts', '*.spec.js'],
-      rules: {
-        'no-unused-expressions': 'off'
-      }
-    }
+    'plugin:import/recommended'
   ],
   rules: {
     // import
@@ -108,13 +16,11 @@ module.exports = {
     'import/no-unresolved': 'off',
     'import/no-absolute-path': 'off',
 
-    // Common
+    // common
     'semi': ['error', 'never'],
-    'curly': ['error', 'multi-or-nest', 'consistent'],
     'quotes': ['error', 'single'],
     'quote-props': ['error', 'consistent-as-needed'],
     'no-unused-vars': 'warn',
-    'no-param-reassign': 'off',
     'array-bracket-spacing': ['error', 'never'],
     'brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
     'block-spacing': ['error', 'always'],
@@ -153,7 +59,7 @@ module.exports = {
       {
         allowNamedFunctions: false,
         allowUnboundThis: true
-      }
+      },
     ],
     'object-shorthand': [
       'error',
@@ -197,47 +103,6 @@ module.exports = {
     'vars-on-top': 'error',
     'require-await': 'off',
     'no-return-assign': 'off',
-    'operator-linebreak': ['error', 'before'],
-
-    // unicorns
-    // Pass error message when throwing errors
-    'unicorn/error-message': 'error',
-    // Uppercase regex escapes
-    'unicorn/escape-case': 'error',
-    // Array.isArray instead of instanceof
-    'unicorn/no-array-instanceof': 'error',
-    // Prevent deprecated `new Buffer()`
-    'unicorn/no-new-buffer': 'error',
-    // Keep regex literals safe!
-    'unicorn/no-unsafe-regex': 'off',
-    // Lowercase number formatting for octal, hex, binary (0x1'error' instead of 0X1'error')
-    'unicorn/number-literal-case': 'error',
-    // ** instead of Math.pow()
-    'unicorn/prefer-exponentiation-operator': 'error',
-    // includes over indexOf when checking for existence
-    'unicorn/prefer-includes': 'error',
-    // String methods startsWith/endsWith instead of more complicated stuff
-    'unicorn/prefer-starts-ends-with': 'error',
-    // textContent instead of innerText
-    'unicorn/prefer-text-content': 'error',
-    // Enforce throwing type error when throwing error while checking typeof
-    'unicorn/prefer-type-error': 'error',
-    // Use new when throwing error
-    'unicorn/throw-new-error': 'error',
-
-    'no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
-    'eslint-comments/disable-enable-pair': 'off',
-    'import/no-named-as-default-member': 'off',
-
-    'sort-imports': [
-      'error',
-      {
-        ignoreCase: false,
-        ignoreDeclarationSort: true,
-        ignoreMemberSort: false,
-        memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        allowSeparatedGroups: false
-      }
-    ]
+    'operator-linebreak': ['error', 'before']
   }
 }
